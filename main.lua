d6e2ec5084e7cd1011810f00a6e1e3ecd7d23ba1
Boris Balaban <boris.balaban@devexpress.com>
2021-06-03 00:19:42 +0300

    Created application's background and background for the game area | Changing the size of screen

--- main.lua
+++ main.lua
@@ -1,7 +1,12 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+local background = display.newRect(display.contentCenterX, display.contentCenterY, display.actualContentWidth, display.actualContentHeight)
+local gradient = {
+    type="gradient",
+    color1={ 0.486, 0.788, 0.956, 1 }, 
+    color2={ 0.176, 0.886, 0.290, 1 }, 
+    direction="down"
+}
+background:setFillColor(gradient)
+background.fill.rotation = 345
 
--- Your code here
+local gameArea = display.newRect(display.contentCenterX, display.contentCenterY - 150, 900, 1500)
+gameArea:setFillColor(0, 0, 0, 1)
